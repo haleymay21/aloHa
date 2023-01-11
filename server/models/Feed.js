ddaa99855b9e46b487548f75be78a4e895200d3a
JacquelineHockin <jacquelinehcashman@gmail.com>
2023-01-10 19:56:16 -1000

    init commit for personal work branch

--- server/models/Feed.js
+++ server/models/Feed.js
@@ -1,22 +1,21 @@
-const { Schema } = require('mongoose');
-const commentsSchema = require('./Comments')
+const { Schema } = require("mongoose");
+const commentsSchema = require("./Comments");
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const feedSchema = new Schema({
-  status:
-    {
-      type: String,
-    },
+  status: {
+    type: String,
+  },
   urgency: {
     type: Number,
     required: true,
-    // add a min and max between 1-5 
+    // add a min and max between 1-5
   },
   // saved book id from GoogleBooks
   resolved: {
     type: Boolean,
     required: true,
   },
-  comments: [commentsSchema]
+  comments: [commentsSchema],
 });
 
 module.exports = feedSchema;
