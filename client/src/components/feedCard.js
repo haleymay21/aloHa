6b8c7fb33a9f0c28210f6045646efcb1b95c6bed
Chase Binkley <chasebinkley@Chases-MBP.lan>
2023-01-12 17:08:11 -1000

    fixes problems with node modules and updates feedCard

--- client/src/components/feedCard.js
+++ client/src/components/feedCard.js
@@ -1,32 +1,28 @@
 // import style package here with specific style components
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card} from "react-bootstrap";
 
 
-
-function feedCard(props){
+function feedCard(props) {
     // space for any variables like useState
 
 
 
     return(
-        <div className="card">
-            <div className="feed-container">
-                <img alt={props.username} src={props.image} />
-            </div>
-            <div className="content">
-                <ul>
-                    <li>
-                        <strong>Name:</strong> {props.name}
-                        {/* firstName.lastName */}
-                    </li>
-                    <li>
-                        <strong>Date:</strong> {props.date}
-                    </li>
-                    <li>
-                        <strong>Status:</strong> {props.status}
-                    </li>
-                </ul>
-            </div>
+        <div className="feed-card">
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>Name:{props.name}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Date:{props.date}</Card.Subtitle>
+                    <Card.Text>
+                        Status:{props.status}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         </div>
-    )
-}
+    );
+    return <div className="feed-card">{props.map(feedCard)}</div>;
+
+};
 module.exports = feedCard
