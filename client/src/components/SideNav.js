53b5ff902af1ed95532d5f5a39723c9aa447dac0
Haley May <haymon24u@gmail.com>
2023-01-15 16:27:03 -1000

    add minor styling

--- client/src/components/SideNav.js
+++ client/src/components/SideNav.js
@@ -7,27 +7,32 @@ const SideNav = () => {
     <>
       <Nav
         className="col-md-12 d-none d-md-block bg-light sidebar"
-        activeKey="/home"
-        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+        activeKey="/"
       >
+        <p id="first-name">FirstName</p> <p id="last-name">LastName</p>
         <div className="sidebar-sticky"></div>
         <Nav.Item>
-          <Nav.Link href="/home">Active</Nav.Link>
+          <Nav.Link href="" className="link">
+            Your Neighbors
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
+          <Nav.Link eventKey="link-1" className="link">
+            Saved Profiles
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-2">Link</Nav.Link>
+          <Nav.Link eventKey="link-2" className="link">
+            Resources
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="disabled" disabled>
-            Disabled
+          <Nav.Link eventKey="link-3" className="link">
+            Events
           </Nav.Link>
         </Nav.Item>
       </Nav>
     </>
   );
 };
-// const SideNav = withRouter(Side);
 export default SideNav;
--- server/models/Feed.js
+++ server/models/Feed.js
@@ -1,22 +1,20 @@
-const { Schema } = require('mongoose');
-const commentsSchema = require('./Comments')
+const { Schema } = require("mongoose");
+const commentsSchema = require("./Comments");
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const feedSchema = new Schema({
-  status:
-    {
-      type: String,
-    },
-  urgency: {
-    type: Number,
-    required: true,
-    // add a min and max between 1-5 
+  status: {
+    type: String,
+  },
+  general: {
+    type: Boolean,
+    required: false,
   },
   // saved book id from GoogleBooks
   resolved: {
     type: Boolean,
-    required: true,
+    required: false,
   },
-  comments: [commentsSchema]
+  comments: [commentsSchema],
 });
 
 module.exports = feedSchema;
