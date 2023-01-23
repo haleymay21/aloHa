9fd6814d88fc000991bf8b229fa489f1a9f12d92
Haley May <haymon24u@gmail.com>
2023-01-23 09:22:12 -1000

    minor changes cleaning up css

--- client/src/components/LocalsCard.js
+++ client/src/components/LocalsCard.js
@@ -28,98 +28,57 @@ const LocalsCard = () => {
         {localsData.map((localsData) => {
           return (
             <MDBCol>
-              <Button
-                id="locals-card-button"
-                onClick={() => setShowModal(true)}
+              <MDBCard
+                handleModalClose={() => setShowModal(false)}
+                className="our-locals"
               >
-                Show Modal
-              </Button>
-              <Modal
-                size="lg"
-                show={showModal}
-                onHide={() => setShowModal(false)}
-                aria-labelledby="locals-card-modal"
-              >
-                <Tab.Container defaultActiveKey="new-card">
-                  <Modal.Header closeButton>
-                    <Modal.Title id="locals-card-modal">
-                      <p id="modal-card-title">
-                        {" "}
-                        &nbsp;&nbsp;Add a New Profile &nbsp;&nbsp;
-                      </p>
-                    </Modal.Title>
-                  </Modal.Header>
-
-                  <Tab.Content>
-                    <Tab.Pane eventKey="new-card">
-                      <Modal.Body>
-                        <MDBCard
-                          handleModalClose={() => setShowModal(false)}
-                          className="our-locals"
-                        >
-                          <MDBCardImage
-                            // src="https://mdbootstrap.com/img/new/standard/city/044.webp"
-                            // src="https://mymodernmet.com/wp/wp-content/uploads/archive/kCNkeHozTY469iw7DRRo_dianakim1.jpg"
-                            // src="https://wehco.media.clients.ellingtoncms.com/timesfreepress/img/photos/2014/09/10/88050b5e08504e7cb859abadc25ae18c_t800.jpg?90232451fbcadccc64a17de7521d859a8f88077d"
-                            src={localsData.image}
-                            alt="..."
-                            position="top"
-                            className="local-images"
-                          />
-                          <MDBCardBody>
-                            <MDBCardTitle className="local-name">
-                              {localsData.name}
-                            </MDBCardTitle>
-                            <div>
-                              <MDBCardText>
-                                <div>
-                                  <h7 className="card-text-prompts">
-                                    Hometown:
-                                  </h7>
-                                  <p class="card-text-answers">
-                                    {localsData.hometown}
-                                  </p>
-                                </div>
-                                <div>
-                                  <h7 className="card-text-prompts">
-                                    What would you like people in the
-                                    neighborhood to know about you?
-                                  </h7>
-                                  <p class="card-text-answers">
-                                    {localsData.whatToKnow}
-                                  </p>
-                                </div>
-                                <div>
-                                  <h7 className="card-text-prompts">
-                                    What things/support would benefit you the
-                                    most?
-                                  </h7>
-                                  <p class="card-text-answers">
-                                    {localsData.support}
-                                  </p>
-                                </div>
-                                <div>
-                                  <h7 className="card-text-prompts">
-                                    Where can your community members find you?
-                                  </h7>
-                                  <p class="card-text-answers">
-                                    {localsData.whereAreYou}
-                                  </p>
-                                </div>
-                              </MDBCardText>
-                            </div>
-                          </MDBCardBody>
-                          <MDBCardFooter>
-                            <small className="text-muted">
-                              Last updated 3 mins ago
-                            </small>
-                          </MDBCardFooter>
-                        </MDBCard>
-                      </Modal.Body>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </Modal>
+                <MDBCardImage
+                  // src="https://mdbootstrap.com/img/new/standard/city/044.webp"
+                  // src="https://mymodernmet.com/wp/wp-content/uploads/archive/kCNkeHozTY469iw7DRRo_dianakim1.jpg"
+                  // src="https://wehco.media.clients.ellingtoncms.com/timesfreepress/img/photos/2014/09/10/88050b5e08504e7cb859abadc25ae18c_t800.jpg?90232451fbcadccc64a17de7521d859a8f88077d"
+                  src={localsData.image}
+                  alt="..."
+                  position="top"
+                  className="local-images"
+                />
+                <MDBCardBody>
+                  <MDBCardTitle className="local-name">
+                    {localsData.name}
+                  </MDBCardTitle>
+                  <div>
+                    <MDBCardText>
+                      <div>
+                        <h7 className="card-text-prompts">Hometown:</h7>
+                        <p class="card-text-answers">{localsData.hometown}</p>
+                      </div>
+                      <div>
+                        <h7 className="card-text-prompts">
+                          What would you like people in the neighborhood to know
+                          about you?
+                        </h7>
+                        <p class="card-text-answers">{localsData.whatToKnow}</p>
+                      </div>
+                      <div>
+                        <h7 className="card-text-prompts">
+                          What things/support would benefit you the most?
+                        </h7>
+                        <p class="card-text-answers">{localsData.support}</p>
+                      </div>
+                      <div>
+                        <h7 className="card-text-prompts">
+                          Where can your community members find you?
+                        </h7>
+                        <p class="card-text-answers">
+                          {localsData.whereAreYou}
+                        </p>
+                      </div>
+                    </MDBCardText>
+                  </div>
+                </MDBCardBody>
+                <MDBCardFooter>
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </MDBCardFooter>
+              </MDBCard>
             </MDBCol>
           );
         })}
